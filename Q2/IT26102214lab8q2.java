62a776b9d5438f02d5147fd54caa06d27184cb01
IT26102214 <it26102214@my.sliit.lk>
2026-09-19 10:09:59 +0530

    Add files via upload

---
+++ Q2/IT26102214lab8q2.java
@@ -0,0 +1,33 @@
+public class IT26102214lab8q2{ 
+    public static void main(String[] args){
+		int A[]={10,20,30,40,50};
+		int B[]={34,67,12,89,12};
+		int C[]= new int[5];
+		int i;
+		
+		for(i=0;i<A.length;i++){
+			C[i]=A[i]+B[i];
+		}
+		System.out.println("A Array content:");
+		for(i=0;i<A.length;i++){
+			System.out.print(A[i]+" ");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.print("B Array content :");
+		for(i=0;i<B.length;i++){
+			System.out.print(B[i]+" ");
+		}
+		System.out.println();
+		System.out.println();
+		System.out.println("C Array content:" );
+		for(i=0;i<C.length;i++){
+			System.out.print(C[i]+" ");
+		}
+		
+		
+	}
+}
+		
+		
+	  
